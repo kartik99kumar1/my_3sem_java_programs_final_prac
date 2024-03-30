b35c86ebcb3f84a2ea481152a72b5cf7c5758ac2
kartik99kumar1 <codekartik1@gmail.com>
2024-03-30 16:18:27 +0530

    Array Decleration

---
+++ Array_1.java
@@ -0,0 +1,11 @@
+package my_3sem_java_programs_final_prac;
+
+public class Array_1 {
+    public static void main(String[] args) {
+        // ONLY ONE IS POSSIBLE.
+
+        //int arr[] = new int[10];
+        //int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    }
+}
