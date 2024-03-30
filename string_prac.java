a04db96bc69f0255ecff29cfc67ef23fdcc0a4ee
kartik99kumar1 <codekartik1@gmail.com>
2024-03-30 14:31:42 +0530

    java chracters basics IO

---
+++ string_prac.java
@@ -0,0 +1,12 @@
+package my_3sem_java_programs_final_prac;
+import java.util.Scanner;
+public class string_prac {
+    public static void main(String[] args) {
+        char s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of s: ");
+        s = sc.next().charAt(0); // for chracter input.
+        System.out.println(s);
+    }
+    
+}
